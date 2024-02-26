9a67e3d8db3f4209d910420a001897cf2fec8baa
web <web@comratings.com>
2024-02-26 20:47:37 +0800

    1.0.1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # echarts-watermark-webpack-plugin
+
+# Usage
+
+```javascript
+const EchartsWatermarkWebpackPlugin = require('echarts-watermark-webpack-plugin');
+
+module.exports = {
+    plugins: [
+      new EchartsWatermarkWebpackPlugin({
+        text: 'Text', size: 100, alpha: 0.3, font: '18px Microsoft Yahei', rotate: 45
+      })
+    ]
+}
+
+```
--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 module.exports = class EchartsWatermarkWebpackPlugin {
     constructor(options) {
+        this.options = Object.assign({
+            text: 'TEXT', size: 100, alpha: 0.08, font: '20px Microsoft Yahei', rotate: 45
+        }, options);
+        this.options.rotate = -this.options.rotate / 180 * Math.PI
     }
 
     apply(compiler) {
-        compiler.hooks.emit.tapAsync('EchartsWatermarkWebpackPlugin', (compilation, callback) => {
+        const regex = /setOption\(.+?\)/g;
+        const options = JSON.stringify(this.options).replaceAll('"', "'");
+        compiler.hooks.emit.tapAsync('AddClickPrefixPlugin', (compilation, callback) => {
             const assets = compilation.assets
             Object.keys(assets).forEach(item => {
-                console.log(item)
+                if (item.endsWith('.js')) {
+                    let source = assets[item].source();
+                    const newSource = source.replace(regex, params => {
+                        const oldInput = /\((.*?)\)/g.exec(params)[1]
+                        const newInput = `setOption((function name() {function getBackgroundCanvas({text, size, alpha, font, rotate}){const waterMarkText = text;const canvas = document.createElement('canvas');const ctx = canvas.getContext('2d');canvas.width = canvas.height = size;ctx.textAlign = 'center';ctx.textBaseline = 'middle';ctx.globalAlpha = alpha;ctx.font = font;ctx.translate(50, 50);ctx.rotate(rotate);ctx.fillText(waterMarkText, 0, 0);return canvas};${oldInput}.backgroundColor={type: 'pattern',image: getBackgroundCanvas(${options}),repeat: 'repeat'};return ${oldInput};})())`
+                        return newInput
+                    })
+                    assets[item] = {
+                        source: () => newSource,
+                        size: () => newSource.length
+                    }
+                }
             })
             callback()
+
         });
     }
 }
